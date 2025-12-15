f41b882f1a902c4ccdfe10c448b54e8b581acc6f
PhuongTT <tranphuongttbn98@gmail.com>
2025-12-15 18:08:51 +0700

    run create flighht round 2

--- src/test/java/tests/test_scripts/api/integration/IntegrationVoucherTest.java
+++ src/test/java/tests/test_scripts/api/integration/IntegrationVoucherTest.java
@@ -20,7 +20,7 @@ public class IntegrationVoucherTest {
     @DataProvider(name = "flowData")
     public Object[][] flowData() throws Exception {
         String excelPath = System.getProperty("user.dir") + "/src/main/resources/input_excel_file/flow_definition.xlsx";
-        String sheetName = "VC_DAI_LY";
+        String sheetName = "VC_CA_NHAN_ROUND2";
 
         List<Map<String, String>> list = FlowDataLoader.readFlows(excelPath, sheetName);
 
